9faf4586474b535f5edc683170875e48ed74e21b
kimsiyeonA <kakasska1212@gmail.com>
2025-02-11 16:01:14 +0900

    accessToken 생성

---
+++ src/main/java/com/ksy/jwt_02/util/Ut.java
@@ -0,0 +1,22 @@
+package com.ksy.jwt_02.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+public class Ut {
+    public static class json {
+        public static Object toStr(Map<String, Object> map) {
+            try {
+                return new ObjectMapper().writeValueAsString(map);
+            } catch (JsonProcessingException e) {
+                return null;
+            }
+        }
+    }
+    ;
+}
--- src/test/java/com/ksy/jwt_02/Jwt02ApplicationTests.java
+++ src/test/java/com/ksy/jwt_02/Jwt02ApplicationTests.java
@@ -1,6 +1,9 @@
 package com.ksy.jwt_02;
 
+import com.ksy.jwt_02.base.jwt.JwtProvider;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +21,8 @@ class Jwt02ApplicationTests {
 
     @Value("${custom.jwt.secretKey}")
     private String  secretKeyPlain;
+    @Autowired
+    private JwtProvider jwtProvider;
 
     @Test
     @DisplayName("secretKey가 존재해야 함")
@@ -31,5 +38,30 @@ class Jwt02ApplicationTests {
         // base64 인코딩 된 키를 이용해서 SecretKey 객체를 만든다.
         SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
         assertThat(secretKey).isNotNull();
+    }
+
+    @Test
+    @DisplayName("JwtProvider 객체로 SecretKey 객체 생성")
+    void t3() {
+        SecretKey secretKey = jwtProvider.getSecretKey();
+        assertThat(secretKey).isNotNull();
+    }
+    @Test
+    @DisplayName("SecretKey 객체는 단 한번만 생성 되어야 함")
+    void t4() {
+        SecretKey secretKey1 = jwtProvider.getSecretKey();
+        SecretKey secretKey2 = jwtProvider.getSecretKey();
+        assertThat(secretKey1 == secretKey2).isTrue();
+    }
+    @Test
+    @DisplayName("accessToken 얻기")
+    void t5() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1L);
+        claims.put("username", "admin");
+        // 현재시각으로부터 5시간의 유효기간을 가지는 토큰 생성
+        String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+        System.out.println("accessToken: " + accessToken);
+        assertThat(accessToken).isNotNull();
     }
 }
